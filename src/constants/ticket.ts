f78a7a263e36a9395c4e777249f9645709893ec1
brambleappmatus <161206934+brambleappmatus@users.noreply.github.com>
2024-12-25 11:18:25 +0100

    Delete src directory

--- src/constants/ticket.ts
+++
@@ -1,5 +0,0 @@
-export const STAGES = ['OPEN', 'WAITING', 'CLOSED', 'ARCHIVED'] as const;
-export const PRIORITIES = ['LOW', 'MEDIUM', 'HIGH'] as const;
-
-export type Stage = typeof STAGES[number];
-export type Priority = typeof PRIORITIES[number];
